6a83e2a551a1400b9f100134082ce5349bfed68f
Dmitrii Raven <oguvan4ik@gmail.com>
2021-03-09 23:19:17 +0200

    initial commit

---
+++ Module_2_6/Module_2_6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Module_2_6
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var starter = new Starter();
+            starter.Run();
+        }
+    }
+}
