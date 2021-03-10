dcde8222cb7f8cb698620323188486c3b87f2cd8
Dmitrii Raven <oguvan4ik@gmail.com>
2021-03-10 14:50:49 +0200

    Added Cars

---
+++ Module_2_6/Module_2_6/Models/Auto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_2_6
+{
+    public class Auto
+    {
+        protected Auto(double price, string name, double weight, double consumption, string colour)
+        {
+            Price = price;
+            Name = name;
+            Weight = weight;
+            Consumption = consumption;
+            Colour = colour;
+        }
+        
+        public double Price { get; }
+
+        public string Name { get; }
+
+        public double Weight { get; }
+
+        public double Consumption { get; }
+
+        public string Colour { get; }
+
+    }
+}
---
+++ Module_2_6/Module_2_6/Models/EasyCar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Module_2_6.Models
+{
+    public abstract class EasyCar : Auto
+    {
+        protected EasyCar(double price, string name, double weight, double consumption, string colour)
+            : base(price, name, weight, consumption, colour)
+        {
+        }
+
+        public abstract int Year { get; }
+       
+    }
+}
